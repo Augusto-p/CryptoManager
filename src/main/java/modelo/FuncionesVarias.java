28ed35694de640e66292bc5965d7c19f24f81ca9
Augusto-p <auguselo77@gmail.com>
2021-11-13 22:10:23 -0300

    Creacion de MonedaDao y TrancionDao

--- src/main/java/modelo/FuncionesVarias.java
+++ src/main/java/modelo/FuncionesVarias.java
@@ -6,7 +6,7 @@ import java.time.format.DateTimeFormatter;
 public class FuncionesVarias {
 
 	
-	public String getDateTime() {
+	public static String getDateTime() {
 		String date = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(LocalDateTime.now());
 		date = date.replace('/', '-');
 		date = date.replace(':', '-');
--- src/main/java/modelo/TransacionDao.java
+++ src/main/java/modelo/TransacionDao.java
@@ -1,6 +1,49 @@
 package modelo;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import modelo.*;
+import entidades.OperacionEnidad;
+
 public class TransacionDao {
 
+	public int agregarCliente(OperacionEnidad ope) {
+		Connection con = Conexion.getConexion();
+		int resultado = -1;
+
+		try {
+			PreparedStatement ps = con.prepareStatement("insert into trans (id_tipo, fecha, nick_mon, cant_mon, prec_tot) values (?,?, ?, ?, ?);",
+					
+					PreparedStatement.RETURN_GENERATED_KEYS);
+			ps.setInt(1, ope.getMode());
+			ps.setString(2, FuncionesVarias.getDateTime());
+			ps.setString(3, ope.getNik());
+			ps.setDouble(4, ope.getCantidad());
+			ps.setDouble(5, ope.getPrice());
+			
+			
+			
+			if (ps.executeUpdate() > 0) {
+				java.sql.ResultSet generatedKeys = ps.getGeneratedKeys();
+				if (generatedKeys.next()) {
+					int primkey = generatedKeys.getInt(1);
+					resultado = primkey;
+				}
+			}
+
+		} catch (SQLException e) {
+			// TODO: handle exception
+		} finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return resultado;
+	}
 	
 }
