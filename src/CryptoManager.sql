85ffbbe112d1b01860381b4b40106fae3a9e544c
Augusto-p <auguselo77@gmail.com>
2021-11-13 22:04:56 -0300

    pequeños canbios en la base de datos

--- src/CryptoManager.sql
+++ src/CryptoManager.sql
@@ -1,59 +1,96 @@
--- --------------------------------------------------------
--- Host:                         192.168.2.223
--- Versión del servidor:         8.0.27-0ubuntu0.20.04.1 - (Ubuntu)
--- SO del servidor:              Linux
--- HeidiSQL Versión:             11.3.0.6295
--- --------------------------------------------------------
+-- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: cryptomanager
+-- ------------------------------------------------------
+-- Server version	8.0.27
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+--
+-- Table structure for table `moneda`
+--
 
--- Volcando estructura de base de datos para CryptoManager
-CREATE DATABASE IF NOT EXISTS `CryptoManager` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `CryptoManager`;
-
--- Volcando estructura para tabla CryptoManager.moneda
-CREATE TABLE IF NOT EXISTS `moneda` (
+DROP TABLE IF EXISTS `moneda`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `moneda` (
   `nick` varchar(6) NOT NULL,
   `nombre` varchar(15) NOT NULL,
   `logo` blob NOT NULL,
   PRIMARY KEY (`nick`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- La exportación de datos fue deseleccionada.
+--
+-- Dumping data for table `moneda`
+--
 
--- Volcando estructura para tabla CryptoManager.tp_trans
-CREATE TABLE IF NOT EXISTS `tp_trans` (
+
+--
+-- Table structure for table `tp_trans`
+--
+
+DROP TABLE IF EXISTS `tp_trans`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tp_trans` (
   `id_tipo` int NOT NULL,
   `nombre` varchar(25) NOT NULL,
   PRIMARY KEY (`id_tipo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- La exportación de datos fue deseleccionada.
+--
+-- Dumping data for table `tp_trans`
+--
 
--- Volcando estructura para tabla CryptoManager.trans
-CREATE TABLE IF NOT EXISTS `trans` (
-  `cant_mon` int NOT NULL,
-  `fecha` date NOT NULL,
-  `id_trans` int NOT NULL,
+LOCK TABLES `tp_trans` WRITE;
+/*!40000 ALTER TABLE `tp_trans` DISABLE KEYS */;
+INSERT INTO `tp_trans` VALUES (-1,'Venta'),(1,'Compra');
+/*!40000 ALTER TABLE `tp_trans` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `trans`
+--
+
+DROP TABLE IF EXISTS `trans`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `trans` (
+  `fecha` datetime NOT NULL,
+  `id_trans` int NOT NULL AUTO_INCREMENT,
   `prec_tot` float NOT NULL,
   `nick_mon` varchar(6) NOT NULL,
   `id_tipo` int NOT NULL,
-  PRIMARY KEY (`fecha`),
+  `cant_mon` float NOT NULL,
+  PRIMARY KEY (`id_trans`),
   KEY `nick_mon` (`nick_mon`),
   KEY `id_tipo` (`id_tipo`),
   CONSTRAINT `trans_ibfk_1` FOREIGN KEY (`nick_mon`) REFERENCES `moneda` (`nick`),
   CONSTRAINT `trans_ibfk_2` FOREIGN KEY (`id_tipo`) REFERENCES `tp_trans` (`id_tipo`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- La exportación de datos fue deseleccionada.
+--
+-- Dumping data for table `trans`
+--
 
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-11-13 21:59:18
