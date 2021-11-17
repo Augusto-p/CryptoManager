6dc3c455ad4d9b64aaff9d17dbb641f32dd4fdeb
Augusto-p <auguselo77@gmail.com>
2021-11-17 20:40:19 -0300

    Creacion de pool de conecion

---
+++ src/main/java/modelo/MonedaPool.java
@@ -0,0 +1,70 @@
+package modelo;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import javax.annotation.Resource;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+import entidades.MonedaEntidad;
+
+public class MonedaPool {
+	@Resource(name = "jdbc/Cryptomanager")
+	private DataSource pisina;
+
+	public Connection MonedaPool() {
+		Connection con=null;
+		try {
+			Context initCtx = new InitialContext();
+			Context envCtx = (Context) initCtx.lookup("java:comp/env");
+			DataSource ds = (DataSource) envCtx.lookup("jdbc/Cryptomanager");
+			con = ds.getConnection();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+	
+	public ArrayList<MonedaEntidad> obternerListaMonedas() {
+		Connection con = this.MonedaPool();
+		ArrayList<MonedaEntidad> listaMonedas = new ArrayList<MonedaEntidad>();
+		try {
+			PreparedStatement ps = con.prepareStatement("select m.nick,m.nombre, m.logo,sum(t.cant_mon * case when tp.nombre =\"compra\" then 1 else -1 end ) as \"Cantidad\",avg(t.prec_tot * case when tp.nombre =\"compra\" then 1 else 0 end /t.cant_mon * case when tp.nombre =\"compra\" then 1 else 0 end ) as \"PPC\" ,avg(t.prec_tot * case when tp.nombre =\"compra\" then 0 else 1 end /t.cant_mon * case when tp.nombre =\"compra\" then 0 else 1 end ) as \"PPV\" from trans as t left join moneda as m on t.nick_mon = m.nick left join tp_trans as tp on t.id_tipo = tp.id_tipo group by nick_mon;");
+			ResultSet resultado = ps.executeQuery();
+			while (resultado.next()) {
+				String nick = resultado.getString("nick");
+				String name = resultado.getString("nombre");
+				String img = resultado.getString("logo");
+				double Cantidad = resultado.getDouble("Cantidad");
+				double PPC = resultado.getDouble("PPV");
+				double PPV = resultado.getDouble("PPV");
+				MonedaEntidad MonedaDao = new MonedaEntidad(nick, name, img, Cantidad, PPC, PPV);
+				listaMonedas.add(MonedaDao);
+			}
+		} catch (SQLException e) {
+		} finally {
+
+		}
+		return listaMonedas;
+	}
+
+	public void agregarMoneda(MonedaEntidad mone) {
+		Connection con = this.MonedaPool();
+		int resultado = -1;
+		try {
+			PreparedStatement ps = con.prepareStatement("insert into moneda (nik, nombre, logo) values(?,?,?)",
+					PreparedStatement.RETURN_GENERATED_KEYS);
+			ps.setString(1, mone.getNik());
+			ps.setString(2, mone.getName());
+			ps.setString(3, mone.getImg());
+			
+
+
+		} catch (SQLException e) {
+		} finally {
+		}
+	}
+}
+
---
+++ src/main/java/modelo/TransacionPool.java
@@ -0,0 +1,77 @@
+package modelo;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.annotation.Resource;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+import entidades.OperacionEnidad;
+
+public class TransacionPool {
+	@Resource(name = "jdbc/Cryptomanager")
+	private DataSource pisina;
+
+	public Connection TransacionPool() {
+		Connection con=null;
+		try {
+			Context initCtx = new InitialContext();
+			Context envCtx = (Context) initCtx.lookup("java:comp/env");
+			DataSource ds = (DataSource) envCtx.lookup("jdbc/Cryptomanager");
+			con = ds.getConnection();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+	
+	public void agregarTrasacion(OperacionEnidad ope) {
+		Connection con = this.TransacionPool();
+		int resultado = -1;
+		try {
+			PreparedStatement ps = con.prepareStatement("insert into trans (id_tipo, fecha, nick_mon, cant_mon, prec_tot) values (?,?, ?, ?, ?);",
+					PreparedStatement.RETURN_GENERATED_KEYS);
+			ps.setString(1, ope.getMode());
+			ps.setString(2, FuncionesVarias.getDateTime());
+			ps.setString(3, ope.getNik());
+			ps.setDouble(4, ope.getCantidad());
+			ps.setDouble(5, ope.getPrice());
+			
+
+		} catch (SQLException e) {
+		} finally {
+		}
+	}
+	public ArrayList<OperacionEnidad> obternerListaTransaciones() {
+		// TODO Auto-generated method stub
+		Connection con = this.TransacionPool();
+		ArrayList<OperacionEnidad> listaOperaciones = new ArrayList<OperacionEnidad>();
+		try {
+			PreparedStatement ps = con.prepareStatement("select t.id_trans as ID, tp.nombre as modo, m.logo, m.nick, m.nombre, t.cant_mon as \"Cantidad\",prec_tot as \"Precio Total\", (prec_tot/cant_mon) as \"Precio Unitario\", fecha from trans as t left join moneda as m on t.nick_mon = m.nick left join tp_trans as tp on t.id_tipo = tp.id_tipo;");
+			ResultSet resultado = ps.executeQuery();
+			while (resultado.next()) {
+				int id = resultado.getInt("ID");
+				String nick = resultado.getString("nick");
+				String nombre = resultado.getString("nombre");
+				String img = resultado.getString("logo");
+				double Cantidad = resultado.getDouble("Cantidad");
+				double PT = resultado.getDouble("Precio Total");
+				double PU = resultado.getDouble("Precio Unitario");
+				String mode = resultado.getString("modo");
+				String date = resultado.getString("fecha");
+				OperacionEnidad opreacionDao = new OperacionEnidad(id, img, nick, Cantidad, PT, date, mode, PU, nombre);
+				listaOperaciones.add(opreacionDao);
+			}
+		} catch (SQLException e) {
+			// TODO: handle exception
+		} finally {
+		}
+		return listaOperaciones;
+	}
+
+}
